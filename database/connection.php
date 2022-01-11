3f2e9f3f8157d1a4aa763bdaa5253dee8a7fab1f
Nadim-Mahmud <mahmudnadim16@gmail.com>
2022-01-11 22:22:04 +0600

    tempo

--- database/connection.php
+++ database/connection.php
@@ -1,30 +1,46 @@
 <?php
 
-    //$dbConfig = config::getInstance()->get('db');
-    function connect(){
-        // $db = new PDO(
-        //     "mysql:host=127.0.0.1; dbname=bookshelf",
-        //     "root",
-        //     ""
-        // );
+    // //$dbConfig = config::getInstance()->get('db');
+    // function connect(){
+    //     $db = new PDO(
+    //         "mysql:host=sql6.freesqldatabase.com; dbname=sql6464972",
+    //         "sql6464972",
+    //         "C1eK6ge5ur"
+    //     );
 
-        // transfering to sqlite
-        $db = new PDO(
-            'sqlite:bookshelf.sqlite'
-        );
+    //     // transfering to sqlite
+    //     // $db = new PDO(
+    //     //     'sqlite:bookshelf.sqlite'
+    //     // );
         
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    //     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
-        if(!$db){
-            echo 'Database not connected!';
-        }
-        return $db;
-    }
+    //     if(!$db){
+    //         echo 'Database not connected!';
+    //     }
+    //     return $db;
+    // }
 
     // $data = $db->query("SELECT * FROM `book` WHERE 1");
 
     // foreach($data as $key => $datum):
     //     var_dump($datum);
     // endforeach;
+
+    function Connect(){
+        $server_name = "sql6.freesqldatabase.com";
+        $username = "sql6464972";
+        $password = "C1eK6ge5ur";
+        $db_name = "sql6464972";
+        try {
+            $conn = new PDO("mysql:host=$server_name;dbname=$db_name", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+          } catch(PDOException $e) {
+            echo $e;
+            return -1;
+          }
+    }
 
 ?>
--- database/createFirst.php
+++ database/createFirst.php
@@ -3,7 +3,6 @@
     function create_if_not_exists($db){
         
         //$db->query("CREATE DATABASE IF NOT EXISTS bookshelf");
-        /**
         $query_st = "CREATE TABLE IF NOT EXISTS books(
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
                         title VARCHAR(250),
@@ -14,9 +13,9 @@
                     )ENGINE=InnoDb;";
         
         $db->query($query_st);
-        */
 
         // for sqlite 
+        /** 
         $query_st = "CREATE TABLE IF NOT EXISTS books(
             id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
             title VARCHAR(250),
@@ -26,7 +25,7 @@
             pages VARCHAR(250)
         )";
         
-        $db->query($query_st);
+        $db->query($query_st); */
         
 
         // $qt = "INSERT INTO books (title, author, available, isbn, pages)
